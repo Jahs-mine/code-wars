ed08926cb9355f75e691eb8986a5b370c0221548
Temiloluwa Jikiemi <temiloluwajik@gmail.com>
2023-08-09 18:21:08 -0700

    add js code

---
+++ js/code.js
@@ -0,0 +1,23 @@
+// WORD REVERSE
+
+// function reverseWord(str ){
+//     return  str.split("").reverse().join("").split(" ").reverse().join(" ");
+// }
+
+// console.log(reverseWord("Hello World"))
+
+// MULTIPLES OF 3 AND 5
+// function solution(num){
+//     let set = (num<=0)? new Set([0]): new Set();
+
+//     for (let index = num-1 ; index > 0; index--) {
+//         if(!((index%3)*(index%5))){
+//             set.add(index);
+//         }
+//     }
+
+
+//     return Array.from(set).reduce((acc,cur)=>acc+cur)
+// }
+
+// console.log(solution(10))
